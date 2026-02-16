b549fd3b0e1d9118223eb4463c75bd5e28d756ea
Andres Vasquez <andres@thenextsecurity.cl>
2026-02-16 19:23:04 -0300

    first commit from branch

---
+++ SubscriptionManager/src/test/java/cl/thenextsecurity/tns/subscriptionmanager/SubscriptionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.thenextsecurity.tns.subscriptionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
